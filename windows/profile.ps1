ac6c8e687918f2e441e8ad84d51e6c0834aff9d7
Daniel Díaz Carrete <diaz.carrete@gmail.com>
2018-06-29 20:01:19 +0200

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -9,4 +9,5 @@ New-Alias which Get-Command
 
 . $PSScriptRoot\_rg.ps1
 
-$env:FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*" --glob "!.svn/*" --glob "!*/target/*"'
+# $env:FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*" --glob "!.svn/*" --glob "!*/target/*"'
+$env:FZF_DEFAULT_COMMAND='rg --files --no-ignore'
