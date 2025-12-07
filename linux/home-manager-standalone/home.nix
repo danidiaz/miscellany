485663e4834a5d8dff66b2a6b41f1f16dd832f7d
Daniel Díaz <diaz_carrete@yahoo.com>
2025-12-07 23:00:37 +0100

    add ghcup path

--- linux/home-manager-standalone/home.nix
+++ linux/home-manager-standalone/home.nix
@@ -97,6 +97,11 @@
     # '';
   };
 
+  # ghcup is installed separately, but we include the path here
+  home.sessionPath = [
+    "$HOME/.ghcup/bin/"
+  ];
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
